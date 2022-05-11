488f36650577249b214a7e8c0920b12f95393efd
Mani <apple@Manis.local>
2022-05-10 22:33:09 -0700

    First Commit

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,123 @@
+import { createContext, useReducer } from "react"
+// import { createRenderer } from "react-dom/test-utils"
+import githubReducer from "./GithubReducer"
+
+const GithubContext = createContext()
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  }
+
+  const [state, dispatch] = useReducer(githubReducer, initialState)
+
+  //Get initial users (testing purposes)
+
+  // Get Search Result
+  // const searchUsers = async (text) => {
+  //   // if (text !== "") {
+  //   setLoading()
+  //   const params = new URLSearchParams({
+  //     q: text,
+  //   })
+  //   const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+  //     headers: {
+  //       Authorization: `token ${GITHUB_TOKEN}`,
+  //     },
+  //   })
+
+  //   const { items } = await response.json()
+
+  //   dispatch({
+  //     type: "GET_USERS",
+  //     payload: items,
+  //   })
+  //   // } else {
+  //   //   dispatch({
+  //   //     type: "CLEAR_USERS",
+  //   //   })
+  //   // }
+  // }
+
+  // // Get User profile
+  // const getUser = async (login) => {
+  //   setLoading()
+
+  //   const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+  //     headers: {
+  //       Authorization: `token ${GITHUB_TOKEN}`,
+  //     },
+  //   })
+
+  //   if (response.status === 404) {
+  //     window.location = "/notfound"
+  //   } else {
+  //     const data = await response.json()
+
+  //     dispatch({
+  //       type: "GET_USER",
+  //       payload: data,
+  //     })
+  //   }
+  // }
+
+  // // Get user repos
+  // const getUserRepos = async (login) => {
+  //   // if (text !== "") {
+  //   setLoading()
+
+  //   const params = new URLSearchParams({
+  //     sort: "created",
+  //     per_page: 10,
+  //   })
+
+  //   const response = await fetch(
+  //     `${GITHUB_URL}/users/${login}/repos?${params}`,
+  //     {
+  //       headers: {
+  //         Authorization: `token ${GITHUB_TOKEN}`,
+  //       },
+  //     }
+  //   )
+
+  //   const data = await response.json()
+
+  //   dispatch({
+  //     type: "GET_REPOS",
+  //     payload: data,
+  //   })
+  // }
+  // } else {
+  //   dispatch({
+  //     type: "CLEAR_USERS",
+  //   })
+  // }
+  //}
+
+  // const clearUsers = () => dispatch({ type: "CLEAR_USERS" })
+  // const setLoading = () => dispatch({ type: "SET_LOADING" })
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+        // users: state.users,
+        // loading: state.loading,
+        // user: state.user,
+        // repos: state.repos,
+        //searchUsers,
+        // clearUsers,
+        // getUser,
+        // getUserRepos,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export default GithubContext
---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <>
+      <h1 className='text-6xl mb-4 text-white'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light text-white'>
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Layout By:
+        <a className='text-white' href='/'>
+          {" "}
+          Mani
+        </a>
+      </p>
+    </>
+  )
+}
+export default About
